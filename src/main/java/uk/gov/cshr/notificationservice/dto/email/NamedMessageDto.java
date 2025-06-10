5919c1825e11c3c4da03bde7c383ce28dbfb19c1
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-06-10 15:56:00 +0100

    Unit test

--- src/main/java/uk/gov/cshr/notificationservice/dto/email/NamedMessageDto.java
+++ src/main/java/uk/gov/cshr/notificationservice/dto/email/NamedMessageDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,4 +17,8 @@ public class NamedMessageDto extends MessageDto {
     @NotEmpty(message = "{message.name.required}")
     protected String name;
 
+    public NamedMessageDto(Map<String, String> personalisation, String recipient, String reference, String name) {
+        super(personalisation, recipient, reference);
+        this.name = name;
+    }
 }
